793d3ab1028ce561d1959276d2d9b4cbeda108ad
khunfloat <60844731+khunfloat@users.noreply.github.com>
2022-12-22 15:26:14 +0700

    add echo

--- create-project.sh
+++ create-project.sh
@@ -1,10 +1,16 @@
+echo "creating python environment"
+
 python3 -m venv venv
 
 source venv/bin/activate
 
 pip install django djangorestframework
 
+echo "starting django project"
+
 django-admin startproject app .
+
+echo "creating api directory"
 
 mkdir api
 
@@ -16,10 +22,16 @@ mv statics/views.py api
 
 mv statics/urls.py api
 
+echo "creating .gitignore"
+
 touch .gitignore
 
 echo 'venv' >> .gitignore
 
-python manage.py runserver
+echo "deleting /statics"
 
 rm -rf statics
+
+echo "run server"
+
+python manage.py runserver
